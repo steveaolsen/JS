1627ad5bbb6081624e070552187012529afeec42
steveaolsen <steveaolsen@gmail.com>
2018-10-22 06:26:32 -0400

    x

--- reactProject/react-tutorial/src/App.js
+++ reactProject/react-tutorial/src/App.js
@@ -3,10 +3,42 @@ import Table from './Table';
 
 
 class App extends Component {
+//this is the main App that is being rendered in index.js by ReactDOM.render()
+//in index.html, there is one element called root, this is targeted by getElementById
+//in index.js and replaces the empty element with the React App.
     render() {
-        return (//notice that we use className not class below, this is JS and not HTML
-            <div className="small-container">
-                <Table />
+
+        const characters = [//a variable, an array of JSON data
+            {
+                'name': 'Charlie',
+                'job': 'Janitor'
+            },
+            {
+                'name': 'Mac',
+                'job': 'Bouncer'
+            },
+            {
+                'name': 'Dee',
+                'job': 'Aspring actress'
+            },
+            {
+                'name': 'Dennis',
+                'job': 'Bartender'
+            },
+            {
+                'name': 'Steve A Olsen',
+                'job': 'Master React Developer'
+
+            }
+        ];
+
+        return (
+        //notice that we use className not class below, this is JS and not HTML.
+        //returns the elements and props below to the Table class, now the Table
+        //class (component) can use the props {characters}, we'll call it with 
+        //props.characterData in the child component of the Table Class in Table.js
+            <div className="container">
+                <Table characterData={characters} />
             </div>
         );
     }
--- reactProject/react-tutorial/src/Table.js
+++ reactProject/react-tutorial/src/Table.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
-function TableHeader() { //non arrow
+function TableHeader() { 
+//non arrow version just for fun
     return (
         <thead>
             <tr>
@@ -11,35 +12,38 @@ function TableHeader() { //non arrow
     );
 }
 
-const TableBody = () => { //arrow
-    return (
-        <tbody>
-            <tr>
-                <td>Charlie</td>
-                <td>Janitor</td>
+const TableBody = props => { 
+//arrow version just for fun
+//remember in arrow functions parens around the parameters, "props",
+// are not required if only one parameter
+    const rows = props.characterData.map((row, index) => {
+    //create a variable caled rows, props is never defined but ia a part of React
+    //which gets the props (properties) of characterData (which got the JSON data from
+    //the characters array in App.js).
+    //.map takes in parameters of the JSON data, each row and an index number,
+    //we will use the index as our key below...
+        return (
+        //creates a table row element for each key, index being the number in the array from
+        //the above .map((row, index)... 
+        //for example, key = index 0, import that row.name, then the row.job, then on to index 1.
+        //this will iterate over the whole data set no matter how many are in it.
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
             </tr>
-            <tr>
-                <td>Mac</td>
-                <td>Bouncer</td>
-            </tr>
-            <tr>
-                <td>Dee</td>
-                <td>Aspiring actress</td>
-            </tr>
-            <tr>
-                <td>Dennis</td>
-                <td>Bartender</td>
-            </tr>
-        </tbody>
-    );
+        );
+    });
+
+    return <tbody>{rows}</tbody>;//returns the rows to the Table component below
 }
 
 class Table extends Component {
     render() {
+        const { characterData } = this.props;
         return (
             <table>
                 <TableHeader />
-                <TableBody />
+                <TableBody characterData={characterData} />
             </table>
         );
     }

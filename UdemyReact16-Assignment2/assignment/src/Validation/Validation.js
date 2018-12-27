ec0d599fc0d50ca65a29f076c6d29cbc724e2587
steveaolsen <steveaolsen@gmail.com>
2018-12-27 15:45:36 -0500

    x

---
+++ UdemyReact16-Assignment2/assignment/src/Validation/Validation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const validation = (props) => {
+
+    let validationMessage = "Text long Enough.";
+
+    if (props.inputLength < 5) {
+        validationMessage = "Text too short!";
+    }
+
+    return (
+        <div>
+            <p>{validationMessage}</p>
+        </div>
+        
+    );
+};
+
+
+
+export default validation;

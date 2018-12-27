0815587ce5005ae82cebb58659007e5943fa1458
steveaolsen <steveaolsen@gmail.com>
2018-12-27 16:50:38 -0500

    x

--- reactCitiesPopulation/react-tutorial/src/Table.js
+++ reactCitiesPopulation/react-tutorial/src/Table.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
 
-function TableHeader() { 
-//non arrow version just for fun
+const TableHeader = () => { 
     return (
         <thead>
             <tr>
-                <th>City #</th>
+                <th>#</th>
                 <th>City</th>
                 <th>Country</th>
                 <th>Population</th>
@@ -17,21 +16,11 @@ function TableHeader() {
     );
 }
 
-const TableBody = props => { 
-//arrow version just for fun
-//remember in arrow functions parens around the parameters, "props",
-// are not required if only one parameter
+const TableBody = (props) => { 
     const rows = props.characterData.map((row, index) => {
-    //create a variable caled rows, props is never defined but ia a part of React
-    //which gets the props (properties) of characterData (which got the JSON data from
-    //the characters array in App.js).
-    //.map takes in parameters of the JSON data, each row and an index number,
-    //we will use the index as our key below...
+    
         return (
-        //creates a table row element for each key, index being the number in the array from
-        //the above .map((row, index)... 
-        //for example, key = index 0, import that row.name, then the row.job, then on to index 1.
-        //this will iterate over the whole data set no matter how many are in it.
+        
             <tr key={index}>
                 <td>{row.CityRank}</td>
                 <td>{row.CityName}</td>
@@ -45,7 +34,7 @@ const TableBody = props => {
         );
     });
 
-    return <tbody>{rows}</tbody>;//returns the rows to the Table component below
+    return <tbody>{rows}</tbody>;
 }
 
 class Table extends Component {
---
+++ reactCitiesPopulation/react-tutorial/src/YourCity.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+
+const YourCity = (props) => {
+
+    return (
+        <div>       
+            <input type='text' value='Your City Population' ></input>
+        </div>
+    );
+
+}
+
+
+
+
+export default YourCity;

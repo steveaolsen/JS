d9c90ec8afc515d35a068c52f0360e6109bf5c93
steveaolsen <steveaolsen@gmail.com>
2018-12-26 21:11:03 -0500

    x

--- UdemyReactPractice/udemy-react/src/App.js
+++ UdemyReactPractice/udemy-react/src/App.js
@@ -12,16 +12,21 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState(
-      {
-        persons: [
-          {name: 'Steve', age: 35},
-          {name: event.target.value, age: 40},
-          {name: 'Frank', age: 50}
-        ]
-      }
-    )
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -51,15 +56,16 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
+          {this.state.persons.map((person, id) => {
              return <Person 
-                click={() => this.deletePersonHandler(index)}
+                click={() => this.deletePersonHandler(id)}
                 name={person.name}
                 age={person.age}
-                key={person.id} /> //use a unique ID, maybe the one from the database you are pulling from
+                key={person.id}  //use a unique ID, maybe the one from the database you are pulling from
+                changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
               
-            </div> 
+          </div> 
           
       );
     }

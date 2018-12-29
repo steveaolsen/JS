cca52fd83ee7529869b3932b182d40255c839b13
steveaolsen <steveaolsen@gmail.com>
2018-12-29 14:06:47 -0500

    x

--- reactCitiesPopulation/react-tutorial/src/App.js
+++ reactCitiesPopulation/react-tutorial/src/App.js
@@ -1,26 +1,56 @@
 import React, {Component} from 'react';
 import Table from './Table';
 import cities from './Cities';
-import YourCity from './YourCity';
+
+
 
 
 class App extends Component {
 
-    cityChanged = (e) => {
-        return e.target.value;
+    state = {
+        userInput: "Enter Your City's Population",
+        userRank: "This Will Display A Rank Once You Input A Number",
+        popArray: [] //figure out a way to make an array of just the 
+                        //population from the city file
     }
 
 
+    popChanger = (event) => {
+        let userPop = event.target.value;
+        userPop = userPop.replace(/,/g, '');
+        this.setState( {userInput: userPop } );
+    }
+              
+    popClear = (event) => {
+        const clearText = "";
+        this.setState( {userInput: clearText } );
+    }
+
+    popRanker = () => {
+        //figure out how to rank the population against the data in Cities
+    }
+
     render() {
 
-    
+        
+        
+
         return (
        
-            <div className = "container">
-                <YourCity />
+            <div>
+                <input 
+                    type='text' 
+                    onClick={this.popClear}
+                    onChange={this.popChanger}
+                    value={this.state.userInput}
+                    />
+                <p>Your City's Population is: {this.state.userInput}</p>
+                <p>{this.state.userRank}</p>
                 <Table characterData = {cities} />
             </div>
         );
+
+
     }
 }
 
